2801ae89b5d097fc272c6fe826451ba1e66d71d1
Alexis Marquet <alexis@alexismarquet.com>
2022-10-04 22:14:05 +0200

    fix new clippies

--- src/serial.rs
+++ src/serial.rs
@@ -41,7 +41,7 @@ pub enum Error {
 }
 
 /// Interrupt event
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Event {
     /// Determines whether an interrupt occurs when a transmit error occurs,
     /// based on the TXERR flag in the FIFOSTAT register.
