4c0a2d06b2581531b1cb425f8e4c894c1e68a9da
Alexis Marquet <allexoll.m@gmail.com>
2022-10-05 09:39:39 +0200

    fix(tests): self test gpio floating (#25)
    * realised that the pins were already pulled differently, so changed used pins, and added test to address that

--- self-tests/tests/gpio-input-floating.rs
+++ self-tests/tests/gpio-input-floating.rs
@@ -1,7 +1,7 @@
 // Required connections:
 //
-// - P3.01 <-> GND
-// - P1.22 <-> VDD
+// - P4.4 <-> GND
+// - P4.5 <-> VDD
 
 #![no_std]
 #![no_main]
@@ -9,14 +9,17 @@
 use defmt_rtt as _; // transport layer for defmt logs
 use lpc546xx_hal::{
     self as _,
-    gpio::{gpio::Pin, Floating, Input},
+    gpio::{
+        gpio::{P4_4, P4_5},
+        Floating, Input,
+    },
 };
 use panic_probe as _; // panicking behavior
 
 struct State {
     // state shared between `#[test]` functions
-    input_ground: Pin<Input<Floating>>,
-    input_vdd: Pin<Input<Floating>>,
+    input_ground: Option<P4_4<Input<Floating>>>,
+    input_vdd: Option<P4_5<Input<Floating>>>,
 }
 
 #[defmt_test::tests]
@@ -31,22 +34,48 @@ mod tests {
         let mut syscon = dp.SYSCON.freeze(Config::fro12m());
         let gpio = dp.GPIO.split(&mut syscon, &mut iocon);
 
-        let input_ground = gpio.pio3_1.into_floating_input().downgrade();
-        let input_vdd = gpio.pio1_22.into_floating_input().downgrade();
+        let input_ground = gpio.pio4_4.into_floating_input();
+        let input_vdd = gpio.pio4_5.into_floating_input();
         State {
-            input_ground,
-            input_vdd,
+            input_ground: Some(input_ground),
+            input_vdd: Some(input_vdd),
         }
     }
 
     #[test]
-    fn ground_is_low(state: &mut State) {
-        assert!(state.input_ground.is_low().unwrap());
+    fn ground_pulled_high_is_low(state: &mut State) {
+        // take the pin out of the state, put into a pull-up input
+        // assert that it is low and put back into the state
+        let input_ground = state.input_ground.take().unwrap();
+        let input_ground = input_ground.into_pull_up_input();
+        assert!(input_ground.is_low().unwrap());
+        state.input_ground = Some(input_ground.into_floating_input());
     }
 
     #[test]
-    fn vdd_is_high(state: &mut State) {
-        assert!(state.input_vdd.is_high().unwrap());
+    fn vdd_pulled_low_is_high(state: &mut State) {
+        // take the pin out of the state, put into a pull-down input
+        // assert that it is high and put back into the state
+        let input_vdd = state.input_vdd.take().unwrap();
+        let input_vdd = input_vdd.into_pull_down_input();
+        assert!(input_vdd.is_high().unwrap());
+        state.input_vdd = Some(input_vdd.into_floating_input());
+    }
+
+    #[test]
+    fn ground_floating_is_low(state: &mut State) {
+        // take the pin out of the state, assert that it is low and put back into the state
+        let input_ground = state.input_ground.take().unwrap();
+        assert!(input_ground.is_low().unwrap());
+        state.input_ground = Some(input_ground);
+    }
+
+    #[test]
+    fn vdd_floating_is_high(state: &mut State) {
+        // take the pin out of the state, assert that it is high and put back into the state
+        let input_vdd = state.input_vdd.take().unwrap();
+        assert!(input_vdd.is_high().unwrap());
+        state.input_vdd = Some(input_vdd);
     }
     #[test]
     fn always_passes() {
